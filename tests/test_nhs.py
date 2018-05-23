5839ce6d5c9786e7893ffde6ed3c639093f5c72f
William Pettersson <william.pettersson@glasgow.ac.uk>
2018-05-23 16:27:41 +0100

    Tests for _constrain_maximal

--- tests/test_nhs.py
+++ tests/test_nhs.py
@@ -14,11 +14,29 @@ def test_instance_with_twocycle_and_threecycle():
     eq_(opt_result.total_score, 2)
 
 
+def test_instance_with_twocycle_and_threecycle_max_matchable():
+    graph, altruists = read_digraph_file("test-fixtures/test1.json")
+    cfg = k_ip.OptConfig(graph, ndds=altruists, max_cycle=3, max_chain=2)
+    cfg._constrain_maximal = True
+    opt_result = k_ip.optimise_picef_nhs(cfg)
+    eq_(len(opt_result.cycles), 1)
+    eq_(opt_result.total_score, 2)
+
+
 def test_instance_with_two_threeway_or_three_twoway():
     graph, altruists = read_digraph_file("test-fixtures/test2.json")
     opt_result = k_ip.optimise_picef_nhs(k_ip.OptConfig(graph, ndds=altruists,
                                                         max_cycle=3,
                                                         max_chain=2))
+    eq_(len(opt_result.cycles), 3)
+    eq_(opt_result.total_score, 6)
+
+
+def test_instance_with_two_threeway_or_three_twoway_max_matchable():
+    graph, altruists = read_digraph_file("test-fixtures/test2.json")
+    cfg = k_ip.OptConfig(graph, ndds=altruists, max_cycle=3, max_chain=2)
+    cfg._constrain_maximal = True
+    opt_result = k_ip.optimise_picef_nhs(cfg)
     eq_(len(opt_result.cycles), 3)
     eq_(opt_result.total_score, 6)
 
@@ -32,11 +50,31 @@ def test_instance_with_threeway_with_backarc():
     eq_(opt_result.total_score, 3)
 
 
+def test_instance_with_threeway_with_backarc_max_matchable():
+    graph, altruists = read_digraph_file("test-fixtures/test3.json")
+    cfg = k_ip.OptConfig(graph, ndds=altruists, max_cycle=3, max_chain=2)
+    cfg._constrain_maximal = True
+    opt_result = k_ip.optimise_picef_nhs(cfg)
+    eq_(len(opt_result.cycles), 1)
+    eq_(opt_result.total_score, 3)
+
+
 def test_instance_with_threeway_with_backarc_or_threeway_without():
     graph, altruists = read_digraph_file("test-fixtures/test4.json")
     opt_result = k_ip.optimise_picef_nhs(k_ip.OptConfig(graph, ndds=altruists,
                                                         max_cycle=3,
                                                         max_chain=2))
+    eq_(len(opt_result.cycles), 1)
+    eq_(opt_result.total_score, 3)
+    cycle = [v.patient_id() for v in opt_result.cycles[0]]
+    eq_(cycle, [1, 2, 4])
+
+
+def test_instance_with_threeway_with_backarc_or_threeway_without_max_matchable():
+    graph, altruists = read_digraph_file("test-fixtures/test4.json")
+    cfg = k_ip.OptConfig(graph, ndds=altruists, max_cycle=3, max_chain=2)
+    cfg._constrain_maximal = True
+    opt_result = k_ip.optimise_picef_nhs(cfg)
     eq_(len(opt_result.cycles), 1)
     eq_(opt_result.total_score, 3)
     cycle = [v.patient_id() for v in opt_result.cycles[0]]
